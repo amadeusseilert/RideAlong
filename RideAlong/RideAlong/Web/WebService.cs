704679fad5c18f370786b1f36a2e1b6de388aad0
amadeus.seilert <amadeus.seilert@gmail.com>
2016-06-02 18:55:51 -0300

    Criado uma Task que faz um web request do tipo GET. Basta na tela invocar ela no formato "var x = JArray.Parse(await getRequest(URL)"

---
+++ RideAlong/RideAlong/Web/WebService.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace RideAlong.Web
+{
+    class WebService
+    {
+        public static async Task<string> getRequest(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.ContentType = "application/json";
+            string received;
+
+            using (var response = (HttpWebResponse)(await Task<WebResponse>.Factory.FromAsync(request.BeginGetResponse, request.EndGetResponse, null)))
+            {
+                using (var responseStream = response.GetResponseStream())
+                {
+                    using (var sr = new StreamReader(responseStream))
+                    {
+
+                        received = await sr.ReadToEndAsync();
+                    }
+                }
+            }
+
+            return received;
+        }
+    }
+}
