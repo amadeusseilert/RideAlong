7f2de8e7b1fe9f1c8e200e23484ba8ba58645383
amadeus.seilert <amadeus.seilert@gmail.com>
2016-06-06 23:24:58 -0300

    Exemplo de request GET na tela FindRides

--- RideAlong/RideAlong/Views/FindRide.xaml.cs
+++ RideAlong/RideAlong/Views/FindRide.xaml.cs
@@ -16,9 +16,43 @@ namespace RideAlong
     public partial class FindRide : ContentPage
     {
 
+
         public FindRide()
         {
             InitializeComponent();
+
+            Button button = new Button()
+            {
+                Text = "Find Locations!"
+            };
+
+            Label header = new Label
+            {
+                Text = "Exemplo de Request",
+                HorizontalOptions = LayoutOptions.Center
+            };
+
+
+            Label label = new Label();
+
+            button.Clicked += async (sender, e) =>
+            {
+                string result = await Web.WebService.getRequest("http://ec2-52-67-5-21.sa-east-1.compute.amazonaws.com/RideAlong-WebService/api/locations");
+                //HANDLE JSON
+                //...
+                //
+                label.Text = result;
+            };
+
+            Content = new StackLayout
+            {
+                Children =
+                {
+                    header,
+                    button,
+                    label
+                }
+            };
 
         }
     }   
